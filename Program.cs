f64d41b6af56b394c2d7be6233cb5bff7263ccbb
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-01-16 18:58:56 +0300

    Homework added

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExtensionsWithParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string deneme = "merhaba";
+            deneme.IsValidPassword(char.IsDigit(deneme, 0), char.IsUpper(deneme,0));
+
+        }
+    }
+    public static class MyExtensions
+    {
+        public static void IsValidPassword(this string str, params bool[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if ((bool)logic == false)
+                {
+                    Console.WriteLine("Şifre gerekli şartları sağlamıyor");
+                }
+            }
+
+        }
+    }
+}
+
